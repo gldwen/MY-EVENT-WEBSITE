d18b75c42976ccec9eaa174deda0699d26403299
gldwen <980222385@qq.com>
2024-05-24 06:15:25 +1000

    update music bar
    update music bar by js

---
+++ src/website_implementation/js/site_interactions.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    //add src to music lable
+    document.querySelector(".audio").src = "media/music1.mp3";
+    //get the name of music
+    document.querySelector(".musicname").innerHTML ="music1";
+}
